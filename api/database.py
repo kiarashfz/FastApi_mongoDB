eb523b56dce024bb48203c33f5b23b1d0f884461
Kiarash Fazli <myrad.fazli@gmail.com>
2022-10-19 17:43:44 +0330

    mongodb configurations added to database file

--- api/database.py
+++ api/database.py
@@ -1,9 +1,11 @@
 import motor.motor_asyncio
-
+import certifi
 from api.config import settings
 
 mongodb_url = settings.mongodb_url
-client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_url)
+client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_url, tlsCAFile=certifi.where())
 
-db = client.fastapi_mongodb
-users_collection = db.get_collection("users")
+
+def db():
+    mongodb = client.fastapi_mongodb
+    return mongodb
