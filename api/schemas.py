b16c987281b50c4a20ad01f71695100832c56d05
Kiarash Fazli <myrad.fazli@gmail.com>
2022-10-19 17:26:00 +0330

    ObjectId type set to str

--- api/schemas.py
+++ api/schemas.py
@@ -3,7 +3,7 @@ from bson import ObjectId
 
 
 class User(BaseModel):
-    id: ObjectId = Field(default_factory=ObjectId, alias="_id")
+    id: str = Field(default_factory=ObjectId, alias="_id")
     username: str = Field(...)
     email: EmailStr = Field(...)
     password: str = Field(...)
@@ -17,5 +17,23 @@ class User(BaseModel):
                 "username": "kiarash",
                 "email": "kiarash@kiarash.com",
                 "password": "secret"
+            }
+        }
+
+
+class UserResponse(BaseModel):
+    id: str = Field(default_factory=ObjectId, alias="_id")
+    username: str = Field(...)
+    email: EmailStr = Field(...)
+
+    class Config:
+        json_encoders = {ObjectId: str}
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        schema_extra = {
+            "example": {
+                "_id": "id",
+                "username": "kiarash",
+                "email": "kiarash@kiarash.com",
             }
         }
