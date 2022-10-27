0f6f3dcea1587b8a1aa4c2bee75f881c1a0e7d75
Kiarash Fazli <myrad.fazli@gmail.com>
2022-10-27 18:24:36 +0330

    redis configurations added to database file

--- api/database.py
+++ api/database.py
@@ -1,6 +1,7 @@
 import motor.motor_asyncio
 import certifi
 from api.config import settings
+from redis import Redis
 
 mongodb_url = settings.mongodb_url
 client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_url, tlsCAFile=certifi.where())
@@ -9,3 +10,9 @@ client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_url, tlsCAFile=certifi.w
 def get_db():
     db = client.fastapi_mongodb
     return db
+
+
+redis_host = settings.redis_host
+redis_port = settings.redis_port
+redis_db = settings.redis_db
+redis_conn = Redis(host=redis_host, port=redis_port, db=redis_db, decode_responses=True)
