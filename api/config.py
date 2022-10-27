c5ce058ce3684e0df71ec65736c8bcfebe2942b3
Kiarash Fazli <myrad.fazli@gmail.com>
2022-10-27 16:14:34 +0330

    regexes & authjwt env variables included

--- api/config.py
+++ api/config.py
@@ -4,6 +4,15 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     mongodb_url: str
 
+    email_regex: str
+    username_regex: str
+
+    authjwt_secret_key: str
+    authjwt_access_token_expires: int
+    authjwt_refresh_token_expires: int
+    authjwt_denylist_enabled: bool = True
+    authjwt_denylist_token_checks: set = {"access", "refresh"}
+
     class Config:
         env_file = ".env"
 
