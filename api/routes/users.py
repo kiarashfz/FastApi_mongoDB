54de3b2c96291b2fe641323b47f83489e7be0278
Kiarash Fazli <myrad.fazli@gmail.com>
2022-10-27 18:22:25 +0330

    get me user route created

--- api/routes/users.py
+++ api/routes/users.py
@@ -1,26 +1,21 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.encoders import jsonable_encoder
-from pymongo.errors import DuplicateKeyError
-
+from pymongo.errors import NetworkTimeout
+from fastapi_jwt_auth import AuthJWT
 from api import schemas
-from api.database import db
-from api.utils import password_hasher
+from api.database import get_db
 
 router = APIRouter(
-    tags=["User routes"]
+    tags=["User routes"],
+    prefix="/users"
 )
 
 
-@router.post('/sign-up', status_code=status.HTTP_201_CREATED, response_description='User successfully registered.',
-             response_model=schemas.UserResponse)
-async def sign_up(user: schemas.User, mongodb=Depends(db)):
+@router.get('/me', status_code=status.HTTP_200_OK, response_model=schemas.UserResponse)
+async def get_me(Authorize: AuthJWT = Depends(), db=Depends(get_db)):
+    Authorize.jwt_required()
+    current_user_id = Authorize.get_jwt_subject()
     try:
-        user.password = password_hasher(user.password)
-        user = jsonable_encoder(user)
-        await mongodb.users.insert_one(user)
-        return user
-    # username & email fields have unique indexes so DuplicateKeyError may be raised
-    except DuplicateKeyError as e:
-        error_field_name = next(iter(e.details["keyPattern"]))
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail=f'{error_field_name.title()} already exists.')
+        current_user = await db.users.find_one({'_id': current_user_id})
+    except NetworkTimeout:
+        raise HTTPException(status_code=status.HTTP_408_REQUEST_TIMEOUT, detail="Request timed out.")
+    return current_user
